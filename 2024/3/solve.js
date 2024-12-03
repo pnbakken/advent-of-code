6a277b370f78f9487a339370d0a63562e684e58f
pnbakken <74681160+pnbakken@users.noreply.github.com>
2024-12-03 11:24:32 +0100

    day 3 part 2 solved

--- 2024/3/solve.js
+++ 2024/3/solve.js
@@ -5,26 +5,33 @@ import fetchUtil from "../../util/fetchUtil.js";
   const DAY = 3;
   let sum = 0;
 
-  const pattern = /mul\(\d+,\d+\)/g;
+  const commands = /do\(\)|don't\(\)|mul\(\d+,\d+\)/g;
+  let enabled = true;
 
   try {
     const rawData = await fetchUtil(YEAR, DAY);
     console.log(rawData);
     if (rawData) {
-      const matches = rawData.match(pattern);
+      const matches = rawData.match(commands);
       console.log(matches);
 
       if (matches) {
         matches.forEach(function (match, i) {
-          const a = match.split("(")[1].split(",")[0];
-          const b = match.split(")")[0].split(",")[1];
-          console.log("AAAAA:::::", a);
-          console.log("BBBBB:::::", b);
-          console.log("_____________________");
+          if (match === "do()") {
+            enabled = true;
+          } else if (match === "don't()") {
+            enabled = false;
+          } else if (enabled && match.startsWith("mul")) {
+            const a = match.split("(")[1].split(",")[0];
+            const b = match.split(")")[0].split(",")[1];
+            console.log("AAAAA:::::", a);
+            console.log("BBBBB:::::", b);
+            console.log("_____________________");
 
-          const product = Number(a) * Number(b);
-          console.log(product);
-          sum += product;
+            const product = Number(a) * Number(b);
+            console.log(product);
+            sum += product;
+          }
         });
       }
     }
